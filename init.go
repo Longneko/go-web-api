f43cbe1dd14eb037cdd570fdd145c1ecb55126c1
Longneko <valeriy.gul@gmail.com>
2019-03-03 10:39:41 +0200

    Added init.go that creates necessary storage dirs and files.

---
+++ init.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "./auth"
+)
+
+const readAndWriteMode = 0644
+
+func main() {
+    // Create session temp storage path
+    if err := os.Mkdir(auth.SessionStoragePath, readAndWriteMode); err != nil {
+        fmt.Printf("Error creating session storage dir: '%s'\n", err.Error())
+    }
+
+    if err := auth.InitUsers(); err != nil {
+        fmt.Printf("Error creating user storage file: '%s'\n", err.Error())
+    }
+}
